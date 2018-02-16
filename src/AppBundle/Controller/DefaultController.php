e5153392bff2496b742a3a03fba034f8c72c7d61
Dessauges Antoine <dessauges.antoine@gmail.com>
2018-02-16 11:21:29 +0100

    Fix fb likes

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,27 +4,30 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
-        
+    public function indexAction(){
+
+        $app_id = $this->container->getParameter('facebook')['app_id'];
+        $app_secret = $this->container->getParameter('facebook')['app_secret'];
+        $access_token = $this->container->getParameter('facebook')['access_token'];
+
         $fb = new \Facebook\Facebook([
-          'app_id' => '429198470842377',
-          'app_secret' => 'eee2d604f622263e2c5b3fb91ead0738',
+          'app_id' => $app_id,
+          'app_secret' => $app_secret,
           'default_graph_version' => 'v2.12',
         ]);
 
-        $response = $fb->get('/cpnv.ch?fields=fan_count', '429198470842377|sCWL3enKAwhPTB4bhf-8rtyhG-4');
+        $response = $fb->get('/cpnv.ch?fields=fan_count', $access_token);
         $like_count = $response->getGraphNode()['fan_count'];
 
         return $this->render('default/index.html.twig', [
             'like_count' => $like_count
         ]);
+
     }
 }
