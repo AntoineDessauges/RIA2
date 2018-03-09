7b1a9a0a940a5640f98e1cd3197aa90935ce7df8
Dessauges Antoine <dessauges.antoine@gmail.com>
2018-03-09 12:17:51 +0100

    linkedin followers

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
+
 class DefaultController extends Controller
 {
     /**
@@ -16,27 +17,43 @@ class DefaultController extends Controller
 
        
         $fb_like_count = $this->get_fb_likes();
-        $twitter_follow_count = $this->get_twitter_follows();
-        $instagram_follow_count = $this->get_instagram_subscribers();
-        $total_count = $twitter_follow_count + $fb_like_count + $instagram_follow_count;
+        $twitter_followers_count = $this->get_twitter_followers();
+        $instagram_followers_count = $this->get_instagram_subscribers();
+        $linkedin_followers_count = $this->get_linkedin_followers();
+        $total_count = $twitter_followers_count + $fb_like_count + $instagram_followers_count + $linkedin_followers_count;
 
         return $this->render('default/index.html.twig', [
             'fb_like_count' => $fb_like_count,
-            'twitter_follow_count' => $twitter_follow_count,
-            'instagram_follow_count' => $instagram_follow_count,
+            'twitter_followers_count' => $twitter_followers_count,
+            'instagram_followers_count' => $instagram_followers_count,
+            'linkedin_followers_count' => $linkedin_followers_count,
             'total_count' => $total_count
         ]);
 
     }
 
+    function get_linkedin_followers(){
+
+        $url = "https://api.criexe.com/social/pageStats?linkedin=15143046";
+        $json = file_get_contents($url);
+        $jsonObj = json_decode($json);
+        return $jsonObj->linkedin->followers;
+
+    }
+
+
+
     function get_instagram_subscribers(){
+
         $url = "https://www.instagram.com/cpnv.ch/?__a=1";
         $json = file_get_contents($url);
         $jsonObj = json_decode($json);
+
         return $jsonObj->user->followed_by->count;
+
     }
 
-    function get_twitter_follows(){
+    function get_twitter_followers(){
 
         $consumer_key = $this->container->getParameter('twitter')['consumer_key'];
         $consumer_secret = $this->container->getParameter('twitter')['consumer_secret'];
